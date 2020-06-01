7787f42d07fa6884e6c0f371e27ded3d48c7f33d
Eric Le Goff <elegoff@protonmail.com>
2020-06-01 08:55:08 +0200

    WIP : naming rules

--- src/main/java/org/elegoff/plugins/rust/clippy/ClippyRulesDefinition.java
+++ src/main/java/org/elegoff/plugins/rust/clippy/ClippyRulesDefinition.java
@@ -25,7 +25,7 @@ import static org.elegoff.plugins.rust.clippy.ClippySensor.LINTER_NAME;
 
 public class ClippyRulesDefinition implements RulesDefinition {
 
-  private static final String RULES_JSON = "org/elegoff/l10n/rust/rules/rust/clippy/rules.json";
+  private static final String RULES_JSON = "org/elegoff/l10n/rust/rules/clippy/clippylints.json";
   private static final String RULE_REPOSITORY_LANGUAGE = RustLanguage.KEY;
   static final ExternalRuleLoader RULE_LOADER = new ExternalRuleLoader(LINTER_KEY, LINTER_NAME, RULES_JSON, RULE_REPOSITORY_LANGUAGE);
 
