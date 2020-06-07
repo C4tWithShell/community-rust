156917c13feaa5e08e450a7fbb6cbde47c891792
Eric Le Goff <elegoff@protonmail.com>
2020-06-07 18:08:27 +0200

    WIP reorganize grammar

--- rust-frontend/src/main/java/org/sonar/rust/api/RustGrammar.java
+++ rust-frontend/src/main/java/org/sonar/rust/api/RustGrammar.java
@@ -16,7 +16,6 @@ public enum RustGrammar implements GrammarRuleKey {
     EXPRESSION_WITH_BLOCK,
     FILE_INPUT,
     OUTER_ATTRIBUTE,
-
     LITTERAL_EXPRESSION,
     PATH_EXPRESSION,
     OPERATOR_EXPRESSION,
@@ -128,20 +127,82 @@ public enum RustGrammar implements GrammarRuleKey {
     BOOLEAN_LITERAL,
     LIFE_TIME_TOKEN,
     IDENTIFIER_OR_KEYWORD,
-    NON_KEYWORD_IDENTIFIER;
+    NON_KEYWORD_IDENTIFIER,
+    STATEMENT,
+    ITEM,
+    LET_STATEMENT,
+    EXPRESSION_STATEMENT,
+    MACRO_INVOCATIONSEMI,
+    STATEMENTS,
+    ENUM_EXPR_STRUCT,
+    ENUM_EXPR_TUPLE,
+    ENUM_EXPR_FIELDLESS,
+    ENUM_EXPR_FIELD,
+    ENUM_EXPR_FIELDS,
+    PATH_EXPRESSION_SEGMENT,
+    LOOP_LABEL,
+    INFINITE_LOOP_EXPRESSION,
+    PREDICATE_LOOP_EXPRESSION,
+    PREDICATE_PATTERN_LOOP_EXPRESSION,
+    ITERATOR_LOOP_EXPRESSION;
 
     public static Grammar create() {
         LexerfulGrammarBuilder b = LexerfulGrammarBuilder.create();
         b.rule(FILE_INPUT).is(b.zeroOrMore(b.firstOf(NEWLINE, EXPRESSION)), EOF);
-        expressions(b);
         lexical(b);
+        attributes(b);
+        statement(b);
+        expressions(b);
+        types(b);
+
         b.setRootRule(FILE_INPUT);
         return b.buildWithMemoizationOfMatchesForAllRules();
     }
 
+    private static void types(LexerfulGrammarBuilder b) {
+        //1
+        type(b);
+        //5
+        tupletype(b);
+        //13
+        pointer(b);
+        //15
+        trait(b);
+
+
+    }
+
+    public static void statement(LexerfulGrammarBuilder b) {
+        b.rule(STATEMENT).is(b.firstOf(
+                ";",
+                ITEM,
+                LET_STATEMENT,
+                EXPRESSION_STATEMENT,
+                MACRO_INVOCATIONSEMI
+        ));
+    }
+
     /* https://doc.rust-lang.org/reference/expressions.html */
     public static void expressions(LexerfulGrammarBuilder b) {
-
+        literal(b);
+        path(b);
+        block(b);
+        operator(b);
+        grouped(b);
+        array(b);
+        tuple(b);
+        struct(b);
+        enums(b);
+        call(b);
+        methodcall(b);
+        field(b);
+        closure(b);
+        loops(b);
+        range(b);
+        if_expr(b);
+        match(b);
+        return_expr(b);
+        await(b);
         b.rule(EXPRESSION).is(b.firstOf(EXPRESSION_WITHOUT_BLOCK, EXPRESSION_WITH_BLOCK));
         b.rule(EXPRESSION_WITHOUT_BLOCK).is(b.zeroOrMore(OUTER_ATTRIBUTE),
                 b.firstOf(LITTERAL_EXPRESSION,
@@ -175,48 +236,17 @@ public enum RustGrammar implements GrammarRuleKey {
                         IF_LET_EXPRESSION,
                         MATCH_EXPRESSION
                 ));
-        b.rule(BREAK_EXPRESSION).is("break", b.optional(LIFETIME_OR_LABEL), b.optional(EXPRESSION));
+    }
 
-        b.rule(CALL_EXPRESSION).is(b.sequence(EXPRESSION, "(", b.optional(CALL_PARAMS), ")"));
-        b.rule(CALL_PARAMS).is(b.sequence(EXPRESSION,
-                b.zeroOrMore(b.sequence("(", EXPRESSION, ")")), b.optional(",")));
-        b.rule(GROUPED_EXPRESSION).is(b.sequence("(", b.zeroOrMore(INNER_ATTRIBUTE), EXPRESSION));
-        b.rule(PATH_EXPRESSION).is(b.firstOf(PATH_IN_EXPRESSION, QUALIFIED_PATH_IN_EXPRESSION));
-        b.rule(TUPLE_INDEXING_EXPRESSION).is(b.sequence(EXPRESSION, ".", TUPLE_INDEX));
-        b.rule(INDEX_EXPRESSION).is(b.sequence(EXPRESSION, "[", EXPRESSION, "]"));
+    private static void await(LexerfulGrammarBuilder b) {
         b.rule(AWAIT_EXPRESSION).is(b.sequence(EXPRESSION, ".", "await"));
-        b.rule(ARRAY_EXPRESSION).is(b.sequence("[", b.zeroOrMore(INNER_ATTRIBUTE), b.optional(ARRAY_ELEMENTS)));
-        b.rule(ARRAY_ELEMENTS).is(b.firstOf(
-                b.sequence(EXPRESSION, b.zeroOrMore(b.sequence(",", EXPRESSION)), b.optional(",")),
-                b.sequence(EXPRESSION, ";", EXPRESSION)
-        ));
-        b.rule(TUPLE_EXPRESSION).is(b.sequence("(", b.zeroOrMore(INNER_ATTRIBUTE), b.optional(TUPLE_ELEMENT), ")"));
-        b.rule(TUPLE_ELEMENT).is(b.oneOrMore(b.sequence(EXPRESSION, ",")), b.optional(EXPRESSION));
-        structExpressions(b);
-        attributes(b);
-        b.rule(ASYNC_BLOCK_EXPRESSION).is(b.sequence("async", b.optional("move"), BLOCK_EXPRESSION));
-        closures(b);
-        type(b);
-        trait(b);
-        tuple(b);
-        b.rule(UNSAFE_BLOCK_EXPRESSION).is(b.sequence("unsafe", BLOCK_EXPRESSION));
-        pointer(b);
-        b.rule(RETURN_EXPRESSION).is(b.sequence("return", b.optional(EXPRESSION)));
-        b.rule(RANGE_EXPRESSION).is(b.firstOf(
-                RANGE_EXPR,
-                RANGE_FROM_EXPR,
-                RANGE_TO_EXPR,
-                RANGE_FULL_EXPR,
-                RANGE_INCLUSIVE_EXPR,
-                RANGE_TO_INCLUSIVE_EXPR));
-        b.rule(RANGE_EXPR).is(b.sequence(EXPRESSION, "..", EXPRESSION));
-        b.rule(RANGE_FROM_EXPR).is(b.sequence(EXPRESSION, ".."));
-        b.rule(RANGE_TO_EXPR).is(b.sequence("..", EXPRESSION));
-        b.rule(RANGE_FULL_EXPR).is("..");
-        b.rule(RANGE_INCLUSIVE_EXPR).is(b.sequence(EXPRESSION, "..=", EXPRESSION));
-        b.rule(RANGE_TO_INCLUSIVE_EXPR).is(b.sequence("..=", EXPRESSION));
-        paths(b);
+    }
 
+    private static void return_expr(LexerfulGrammarBuilder b) {
+        b.rule(RETURN_EXPRESSION).is(b.sequence("return", b.optional(EXPRESSION)));
+    }
+
+    private static void match(LexerfulGrammarBuilder b) {
         //https://doc.rust-lang.org/reference/expressions/match-expr.html
         b.rule(MATCH_EXPRESSION).is(b.sequence(
                 "match", EXPRESSION, //except struct expressions !!
@@ -248,6 +278,140 @@ public enum RustGrammar implements GrammarRuleKey {
         ));
         b.rule(MATCH_ARM_GUARD).is(b.sequence("if", EXPRESSION));
 
+    }
+
+    private static void if_expr(LexerfulGrammarBuilder b) {
+        b.rule(IF_EXPRESSION).is(b.sequence(
+                "if", EXPRESSION, //except struct expressions !!
+                b.optional(b.sequence(
+                        //else
+                        "else", b.firstOf(BLOCK_EXPRESSION, IF_EXPRESSION, IF_LET_EXPRESSION)
+                ))
+        ));
+        b.rule(IF_LET_EXPRESSION).is(b.sequence(
+                "if", "let", MATCH_ARM_PATTERNS, "=", EXPRESSION, //except struct or lazy boolean operator expression
+                BLOCK_EXPRESSION,
+                b.optional(b.sequence(
+                        //else
+                        "else", b.firstOf(BLOCK_EXPRESSION, IF_EXPRESSION, IF_LET_EXPRESSION)
+                ))
+        ));
+    }
+
+    private static void range(LexerfulGrammarBuilder b) {
+        b.rule(RANGE_EXPRESSION).is(b.firstOf(
+                RANGE_EXPR,
+                RANGE_FROM_EXPR,
+                RANGE_TO_EXPR,
+                RANGE_FULL_EXPR,
+                RANGE_INCLUSIVE_EXPR,
+                RANGE_TO_INCLUSIVE_EXPR));
+        b.rule(RANGE_EXPR).is(b.sequence(EXPRESSION, "..", EXPRESSION));
+        b.rule(RANGE_FROM_EXPR).is(b.sequence(EXPRESSION, ".."));
+        b.rule(RANGE_TO_EXPR).is(b.sequence("..", EXPRESSION));
+        b.rule(RANGE_FULL_EXPR).is("..");
+        b.rule(RANGE_INCLUSIVE_EXPR).is(b.sequence(EXPRESSION, "..=", EXPRESSION));
+        b.rule(RANGE_TO_INCLUSIVE_EXPR).is(b.sequence("..=", EXPRESSION));
+    }
+
+    private static void loops(LexerfulGrammarBuilder b) {
+        b.rule(LOOP_EXPRESSION).is(b.sequence(b.optional(LOOP_LABEL),
+                b.firstOf(
+                        INFINITE_LOOP_EXPRESSION,
+                        PREDICATE_LOOP_EXPRESSION,
+                        PREDICATE_PATTERN_LOOP_EXPRESSION,
+                        ITERATOR_LOOP_EXPRESSION)
+        ));
+        b.rule(INFINITE_LOOP_EXPRESSION).is(b.sequence(
+                "loop",BLOCK_EXPRESSION));
+        b.rule(PREDICATE_LOOP_EXPRESSION).is(b.sequence(
+                "while", EXPRESSION, //except struct expression
+                BLOCK_EXPRESSION
+        ));
+        b.rule(PREDICATE_PATTERN_LOOP_EXPRESSION).is(b.sequence(
+                "while","let",MATCH_ARM_PATTERNS,"=",EXPRESSION, //except struct expression
+                BLOCK_EXPRESSION
+        ));
+        b.rule(ITERATOR_LOOP_EXPRESSION).is(b.sequence(
+                "for",PATTERN,"IN",EXPRESSION, //except struct expression
+                BLOCK_EXPRESSION
+        ));
+        b.rule(LOOP_LABEL).is(b.sequence(LIFE_TIME_OR_LABEL,":"));
+        b.rule(BREAK_EXPRESSION).is(b.sequence("break", b.optional(LIFETIME_OR_LABEL), b.optional(EXPRESSION)));
+        b.rule(CONTINUE_EXPRESSION).is(b.sequence(
+                "continue",b.optional(LIFE_TIME_OR_LABEL)));
+
+
+    }
+
+    private static void field(LexerfulGrammarBuilder b) {
+        b.rule(FIELD_EXPRESSION).is(b.sequence(EXPRESSION, ".", IDENTIFIER));
+    }
+
+    private static void methodcall(LexerfulGrammarBuilder b) {
+        b.rule(METHOD_CALL_EXPRESSION).is(b.sequence(
+                EXPRESSION, ".", PATH_EXPRESSION_SEGMENT,
+                "(", b.optional(CALL_PARAMS), ")"
+        ));
+    }
+
+    private static void call(LexerfulGrammarBuilder b) {
+        b.rule(CALL_EXPRESSION).is(b.sequence(EXPRESSION, "(", b.optional(CALL_PARAMS), ")"));
+        b.rule(CALL_PARAMS).is(b.sequence(EXPRESSION,
+                b.zeroOrMore(b.sequence("(", EXPRESSION, ")")), b.optional(",")));
+
+    }
+
+    /* https://doc.rust-lang.org/reference/expressions/enum-variant-expr.html */
+    private static void enums(LexerfulGrammarBuilder b) {
+        b.rule(ENUMERATION_VARIANT_EXPRESSION).is(b.firstOf(
+                ENUM_EXPR_STRUCT,
+                ENUM_EXPR_TUPLE,
+                ENUM_EXPR_FIELDLESS
+        ));
+        b.rule(ENUM_EXPR_STRUCT).is(b.sequence(PATH_IN_EXPRESSION, "{",
+                b.optional(ENUM_EXPR_FIELDS), "}"
+        ));
+        b.rule(ENUM_EXPR_FIELDS).is(b.sequence(
+                ENUM_EXPR_FIELD,
+                b.zeroOrMore(b.sequence(",", ENUM_EXPR_FIELDS), b.optional("'"))
+        ));
+        b.rule(ENUM_EXPR_FIELD).is(b.firstOf(IDENTIFIER,
+                b.sequence(b.firstOf(IDENTIFIER, TUPLE_INDEX), ":", EXPRESSION)
+        ));
+        b.rule(ENUM_EXPR_TUPLE).is(b.sequence(
+                PATH_IN_EXPRESSION, "(",
+                b.optional(b.sequence(
+                        EXPRESSION,
+                        b.zeroOrMore(b.sequence(",", EXPRESSION)),
+                        b.optional("'")
+                )), ")"
+        ));
+        b.rule(ENUM_EXPR_FIELDLESS).is(PATH_IN_EXPRESSION);
+    }
+
+    private static void tuple(LexerfulGrammarBuilder b) {
+        b.rule(TUPLE_EXPRESSION).is(b.sequence("(", b.zeroOrMore(INNER_ATTRIBUTE), b.optional(TUPLE_ELEMENT), ")"));
+        b.rule(TUPLE_ELEMENT).is(b.oneOrMore(b.sequence(EXPRESSION, ",")), b.optional(EXPRESSION));
+        b.rule(TUPLE_INDEXING_EXPRESSION).is(b.sequence(EXPRESSION, ".", TUPLE_INDEX));
+
+
+    }
+
+    private static void array(LexerfulGrammarBuilder b) {
+        b.rule(ARRAY_EXPRESSION).is(b.sequence("[", b.zeroOrMore(INNER_ATTRIBUTE), b.optional(ARRAY_ELEMENTS)));
+        b.rule(ARRAY_ELEMENTS).is(b.firstOf(
+                b.sequence(EXPRESSION, b.zeroOrMore(b.sequence(",", EXPRESSION)), b.optional(",")),
+                b.sequence(EXPRESSION, ";", EXPRESSION)
+        ));
+        b.rule(INDEX_EXPRESSION).is(b.sequence(EXPRESSION, "[", EXPRESSION, "]"));
+    }
+
+    private static void grouped(LexerfulGrammarBuilder b) {
+        b.rule(GROUPED_EXPRESSION).is(b.sequence("(", b.zeroOrMore(INNER_ATTRIBUTE), EXPRESSION));
+    }
+
+    private static void operator(LexerfulGrammarBuilder b) {
         // https://doc.rust-lang.org/reference/expressions/operator-expr.html
         b.rule(OPERATOR_EXPRESSION).is(b.firstOf(
                 BORROW_EXPRESSION,
@@ -308,6 +472,27 @@ public enum RustGrammar implements GrammarRuleKey {
                 b.sequence(EXPRESSION, "<<=", EXPRESSION),
                 b.sequence(EXPRESSION, ">>=", EXPRESSION)
         ));
+    }
+
+    private static void block(LexerfulGrammarBuilder b) {
+        b.rule(BLOCK_EXPRESSION).is(b.sequence("{", b.zeroOrMore(INNER_ATTRIBUTE),
+                b.optional(STATEMENTS), "}"
+        ));
+        b.rule(STATEMENTS).is(b.firstOf(
+                b.oneOrMore(STATEMENT),
+                b.sequence(b.oneOrMore(STATEMENT), EXPRESSION_WITHOUT_BLOCK),
+                EXPRESSION_WITHOUT_BLOCK
+        ));
+        b.rule(ASYNC_BLOCK_EXPRESSION).is(b.sequence("async", b.optional("move"), BLOCK_EXPRESSION));
+        b.rule(UNSAFE_BLOCK_EXPRESSION).is(b.sequence("unsafe", BLOCK_EXPRESSION));
+
+    }
+
+    private static void path(LexerfulGrammarBuilder b) {
+        b.rule(PATH_EXPRESSION).is(b.firstOf(PATH_IN_EXPRESSION, QUALIFIED_PATH_IN_EXPRESSION));
+    }
+
+    private static void literal(LexerfulGrammarBuilder b) {
         b.rule(LITTERAL_EXPRESSION).is(b.firstOf(
                 CHAR_LITERAL,
                 STRING_LITERAL,
@@ -319,28 +504,11 @@ public enum RustGrammar implements GrammarRuleKey {
                 FLOAT_LITERAL,
                 BOOLEAN_LITERAL
         ));
-        tokens(b);
-        b.rule(IF_EXPRESSION).is(b.sequence(
-                "if",EXPRESSION, //except struct expressions !!
-                b.optional(b.sequence(
-                        //else
-                   "else", b.firstOf(BLOCK_EXPRESSION, IF_EXPRESSION, IF_LET_EXPRESSION)
-                ))
-        ));
-        b.rule(IF_LET_EXPRESSION).is(b.sequence(
-                "if","let",MATCH_ARM_PATTERNS,"=",EXPRESSION, //except struct or lazy boolean operator expression
-                BLOCK_EXPRESSION,
-                b.optional(b.sequence(
-                        //else
-                        "else", b.firstOf(BLOCK_EXPRESSION, IF_EXPRESSION, IF_LET_EXPRESSION)
-                ))
-        ));
-
-
     }
 
+
     /* https://doc.rust-lang.org/reference/expressions/struct-expr.html */
-    public static void structExpressions(LexerfulGrammarBuilder b) {
+    public static void struct(LexerfulGrammarBuilder b) {
         b.rule(STRUCT_EXPRESSION).is(b.firstOf(
                 STRUCT_EXPR_STRUCT,
                 STRUCT_EXPR_TUPLE,
@@ -381,7 +549,7 @@ public enum RustGrammar implements GrammarRuleKey {
     }
 
     /* https://doc.rust-lang.org/reference/expressions/closure-expr.html*/
-    public static void closures(LexerfulGrammarBuilder b) {
+    public static void closure(LexerfulGrammarBuilder b) {
         b.rule(CLOSURE_EXPRESSION).is(b.sequence
                 (b.optional("move"),
                         b.firstOf("||", b.sequence("|", b.optional(CLOSURE_PARAMETERS), "|"))));
@@ -448,7 +616,7 @@ public enum RustGrammar implements GrammarRuleKey {
         b.rule(LIFE_TIME).is(b.firstOf(LIFE_TIME_OR_LABEL, "'static", "'_"));
     }
 
-    public static void tuple(LexerfulGrammarBuilder b) {
+    public static void tupletype(LexerfulGrammarBuilder b) {
         b.rule(TUPLE_INDEX).is(b.firstOf("0", b.sequence(
                 NON_ZERO_DEC_DIGIT, b.zeroOrMore(DEC_DIGIT)
         )));
@@ -467,7 +635,7 @@ public enum RustGrammar implements GrammarRuleKey {
     }
 
     /* https://doc.rust-lang.org/reference/paths.html */
-    public static void paths(LexerfulGrammarBuilder b) {
+    public static void lexicalpath(LexerfulGrammarBuilder b) {
         b.rule(QUALIFIED_PATH_IN_TYPE).is(b.sequence(QUALIFIED_PATH_TYPE, b.oneOrMore(
                 b.sequence("::", TYPE_PATH_SEGMENT)
 
@@ -490,7 +658,7 @@ public enum RustGrammar implements GrammarRuleKey {
         ));
     }
 
-    public static void tokens(LexerfulGrammarBuilder b) {
+    public static void lexicaltoken(LexerfulGrammarBuilder b) {
         b.rule(LIFE_TIME_TOKEN).is(b.firstOf(
                 b.sequence("'", IDENTIFIER_OR_KEYWORD),
                 b.sequence("'", "_")
@@ -499,13 +667,22 @@ public enum RustGrammar implements GrammarRuleKey {
     }
 
     /* https://doc.rust-lang.org/reference/lexical-structure.html */
-    public static void  lexical(LexerfulGrammarBuilder b){
+    public static void lexical(LexerfulGrammarBuilder b) {
+
         b.rule(IDENTIFIER_OR_KEYWORD).is(b.firstOf(
                 regexp(" [a-z A-Z] [a-z A-Z 0-9 _]*"),
                 b.sequence("_", b.oneOrMore(regexp("[a-z A-Z 0-9 _]"))))
-                );
+        );
+
+        lexicaltoken(b);
+        lexicalpath(b);
 
     }
+
+
+
+
+
 
 
 }
