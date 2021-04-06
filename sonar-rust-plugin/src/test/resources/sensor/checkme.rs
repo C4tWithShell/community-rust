1c9c56a6025f958b9f15587a8addd3c1e187924d
Eric Le Goff <elegoff@protonmail.com>
2021-04-06 21:25:22 +0200

    WIP

--- sonar-rust-plugin/src/test/resources/sensor/checkme.rs
+++ sonar-rust-plugin/src/test/resources/sensor/checkme.rs
@@ -1,137 +1,101 @@
-// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
 
-use crate::colors;
-use crate::fs_util::resolve_from_cwd;
-use deno_core::error::custom_error;
-use deno_core::error::uri_error;
-use deno_core::error::AnyError;
-use deno_core::serde::Deserialize;
-use deno_core::serde::Serialize;
-use deno_core::url;
-use deno_core::ModuleSpecifier;
-use std::collections::HashSet;
-use std::fmt;
-use std::hash::Hash;
-#[cfg(not(test))]
-use std::io;
-use std::path::{Path, PathBuf};
-#[cfg(test)]
-use std::sync::atomic::AtomicBool;
-#[cfg(test)]
-use std::sync::atomic::Ordering;
-#[cfg(test)]
-use std::sync::Mutex;
+fn git_commit_hash() -> String {
+    if let Ok(output) = std::process::Command::new("git")
+    {
+        if output.status.success() {
+            std::str::from_utf8(&output.stdout[..40])
 
-const PERMISSION_EMOJI: &str = "⚠️";
-
-/// Tri-state value for storing permission state
-#[derive(PartialEq, Debug, Clone, Copy, Deserialize, PartialOrd)]
-pub enum PermissionState {
-    Granted = 0,
-    Prompt = 1,
-    Denied = 2,
+        } else {
+            // When not in git repository
+            // (e.g. when the user install by `cargo install deno`)
+            "UNKNOWN".to_string()
+        }
+    } else {
+        // When there is no git command for some reason
+        "UNKNOWN".to_string()
+    }
 }
+/*
+fn main() {
+    // Skip building from docs.rs.
+    if env::var_os("DOCS_RS").is_some() {
+        return;
+    }
 
-impl PermissionState {
-    /// Check the permission state.
-    fn check(self, name: &str, info: Option<&str>) -> Result<(), AnyError> {
-        if self == PermissionState::Granted {
-            log_perm_access(&format!(
-                "{} access{}",
-                name,
-                info.map_or(Default::default(), |info| { format!(" to {}", info) }),
+    // To debug snapshot issues uncomment:
+    // op_fetch_asset::trace_serializer();
+
+    println!("cargo:rustc-env=TS_VERSION={}", ts_version());
+    println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_commit_hash());
+    println!(
+        "cargo:rustc-env=DENO_CONSOLE_LIB_PATH={}",
+        deno_console::get_declaration().display()
+    );
+    println!(
+        "cargo:rustc-env=DENO_URL_LIB_PATH={}",
+        deno_url::get_declaration().display()
+    );
+    println!(
+        "cargo:rustc-env=DENO_WEB_LIB_PATH={}",
+        deno_web::get_declaration().display()
+    );
+    println!(
+        "cargo:rustc-env=DENO_FETCH_LIB_PATH={}",
+        deno_fetch::get_declaration().display()
+    );
+    println!(
+        "cargo:rustc-env=DENO_WEBGPU_LIB_PATH={}",
+        deno_webgpu::get_declaration().display()
+    );
+    println!(
+        "cargo:rustc-env=DENO_WEBSOCKET_LIB_PATH={}",
+        deno_websocket::get_declaration().display()
+    );
+    println!(
+        "cargo:rustc-env=DENO_CRYPTO_LIB_PATH={}",
+        deno_crypto::get_declaration().display()
+    );
+
+    println!("cargo:rustc-env=TARGET={}", env::var("TARGET").unwrap());
+    println!("cargo:rustc-env=PROFILE={}", env::var("PROFILE").unwrap());
+    if let Ok(c) = env::var("DENO_CANARY") {
+        println!("cargo:rustc-env=DENO_CANARY={}", c);
+    }
+
+    let c = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let o = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    // Main snapshot
+    let compiler_snapshot_path = o.join("COMPILER_SNAPSHOT.bin");
+
+    let js_files = get_js_files("tsc");
+    create_compiler_snapshot(&compiler_snapshot_path, js_files, &c);
+
+    #[cfg(target_os = "windows")]
+        {
+            let mut res = winres::WindowsResource::new();
+            res.set_icon("deno.ico");
+            res.set_language(winapi::um::winnt::MAKELANGID(
+                winapi::um::winnt::LANG_ENGLISH,
+                winapi::um::winnt::SUBLANG_ENGLISH_US,
             ));
-            return Ok(());
+            res.compile().unwrap();
         }
-        let message = format!(
-            "Requires {} access{}, run again with the --allow-{} flag",
-            name,
-            info.map_or(Default::default(), |info| { format!(" to {}", info) }),
-            name
-        );
-        Err(custom_error("PermissionDenied", message))
-    }
 }
 
-impl fmt::Display for PermissionState {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            PermissionState::Granted => f.pad("granted"),
-            PermissionState::Prompt => f.pad("prompt"),
-            PermissionState::Denied => f.pad("denied"),
-        }
-    }
+fn get_js_files(d: &str) -> Vec<PathBuf> {
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let mut js_files = std::fs::read_dir(d)
+        .unwrap()
+        .map(|dir_entry| {
+            let file = dir_entry.unwrap();
+            manifest_dir.join(file.path())
+        })
+        .filter(|path| path.extension().unwrap_or_default() == "js")
+        .collect::<Vec<PathBuf>>();
+    js_files.sort();
+    js_files
 }
 
-impl Default for PermissionState {
-    fn default() -> Self {
-        PermissionState::Prompt
-    }
-}
 
-#[derive(Clone, Debug, Default, PartialEq)]
-pub struct UnitPermission {
-    pub name: &'static str,
-    pub description: &'static str,
-    pub state: PermissionState,
-}
-
-impl UnitPermission {
-    pub fn query(&self) -> PermissionState {
-        self.state
-    }
-
-    pub fn request(&mut self) -> PermissionState {
-        if self.state == PermissionState::Prompt {
-            if permission_prompt(&format!("access to {}", self.description)) {
-                self.state = PermissionState::Granted;
-            } else {
-                self.state = PermissionState::Denied;
-            }
-        }
-        self.state
-    }
-
-    pub fn revoke(&mut self) -> PermissionState {
-        if self.state == PermissionState::Granted {
-            self.state = PermissionState::Prompt;
-        }
-        self.state
-    }
-
-    pub fn check(&self) -> Result<(), AnyError> {
-        self.state.check(self.name, None)
-    }
-}
-
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct UnaryPermission<T: Eq + Hash> {
-    #[serde(skip)]
-    pub name: &'static str,
-    #[serde(skip)]
-    pub description: &'static str,
-    pub global_state: PermissionState,
-    pub granted_list: HashSet<T>,
-    pub denied_list: HashSet<T>,
-}
-
-#[derive(Clone, Eq, PartialEq, Hash, Debug, Default, Deserialize)]
-pub struct ReadDescriptor(pub PathBuf);
-
-#[derive(Clone, Eq, PartialEq, Hash, Debug, Default, Deserialize)]
-pub struct WriteDescriptor(pub PathBuf);
-
-#[derive(Clone, Eq, PartialEq, Hash, Debug, Default, Deserialize)]
-pub struct NetDescriptor(pub String, pub Option<u16>);
-
-impl NetDescriptor {
-
-
-    pub fn from_string(host: String) -> Self {
-        let url = url::Url::parse(&format!("http://{}", host)).unwrap();
-        let hostname = url.host_str().unwrap().to_string();
-
-        NetDescriptor(hostname, url.port())
-    }
-}
-
+ */
