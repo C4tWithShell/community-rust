3611f946593ee0b01e0b68e09390ec7df9d6a054
Eric Le Goff <elegoff@protonmail.com>
2021-04-30 21:50:30 +0200

    Remove some FIXME

--- rust-frontend/src/test/java/org/sonar/rust/parser/expressions/RangeExpressionTest.java
+++ rust-frontend/src/test/java/org/sonar/rust/parser/expressions/RangeExpressionTest.java
@@ -38,7 +38,7 @@ public class RangeExpressionTest {
         assertThat(RustGrammar.create().build().rule(RustGrammar.EXPRESSION))
                 .matches("1..2")
                 .matches("start..end")
-                //FIXME .matches("0..(4 - (len & 3)) & 3")
+                .matches("0..(4 - (len & 3)) & 3")
 
         ;
     }
@@ -91,29 +91,30 @@ public class RangeExpressionTest {
 //        assertThat(RustGrammar.create().build().rule(RustGrammar.RANGE_EXPRESSION))
         assertThat(RustGrammar.create().build().rule(RustGrammar.EXPRESSION))
                 .matches("1..2")// std::ops::Range
-
                 .matches("..4")// std::ops::RangeTo
                 .matches("..")// std::ops::RangeFull
                 .matches("3..")// std::ops::RangeFrom
                 .matches("..=7")// std::ops::RangeToInclusive
                 .matches("0..top()")
-                //FIXME.matches("0..(4 - (len & 3)) & 3")
-
+                .matches("0..(4 - (len & 3)) & 3")
                 .matches("5..=6")// std::ops::RangeInclusive
         ;
     }
 
     @Test
     public void testParsing(){
+
+        //Print out Ast node content for debugging purpose
+
         ParserAdapter<LexerlessGrammar> parser = new ParserAdapter<>(StandardCharsets.UTF_8,RustGrammar.create().build());
         AstNode rootNode = parser.parse("let a = 1..5;");
-        System.out.printf("[ELG]" + rootNode.getType());
+        System.out.printf("[DEBUG]" + rootNode.getType());
         Assertions.assertThat(rootNode.getType()).isSameAs(RustGrammar.COMPILATION_UNIT);
         AstNode astNode = rootNode;
         Assertions.assertThat(astNode.getNumberOfChildren()).isEqualTo(4);
-        System.out.println("[ELG]" + astNode.getType());
+        System.out.println("[DEBUG]" + astNode.getType());
         for (AstNode n : astNode.getChildren()){
-            System.out.println("[ELG] child" + n.getType());
+            System.out.println("[DEBUG] child" + n.getType());
         }
         AstNode st = astNode.getChildren().get(1);
         System.out.println("[ELG] st is " + st);
