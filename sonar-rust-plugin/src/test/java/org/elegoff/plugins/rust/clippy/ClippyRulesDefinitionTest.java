8b34ab1c2e5361d776a143a1921e75ffc7293084
Eric Le Goff <elegoff@protonmail.com>
2021-05-18 17:55:12 +0200

    adapt unit test after refreshing list of clips lints

--- sonar-rust-plugin/src/test/java/org/elegoff/plugins/rust/clippy/ClippyRulesDefinitionTest.java
+++ sonar-rust-plugin/src/test/java/org/elegoff/plugins/rust/clippy/ClippyRulesDefinitionTest.java
@@ -38,11 +38,11 @@ public class ClippyRulesDefinitionTest {
         assertThat(repository.name()).isEqualTo("Clippy");
         assertThat(repository.language()).isEqualTo("rust");
         assertThat(repository.isExternal()).isEqualTo(true);
-        assertThat(repository.rules().size()).isEqualTo(373);
+        assertThat(repository.rules().size()).isEqualTo(459);
 
         RulesDefinition.Rule rule = repository.rule("clippy::absurd_extreme_comparisons");
         assertThat(rule).isNotNull();
-        assertThat(rule.name()).isEqualTo("Checks for comparisons where one side of the relation is either the minimum or maximum value for its type");
+        assertThat(rule.name()).isEqualTo("Checks for comparisons where one side of the relation is\neither the minimum or maximum value for its");
         assertThat(rule.htmlDescription()).isEqualTo("See description of Clippy rule <code>clippy::absurd_extreme_comparisons</code> at" +
                 " the <a href=\"https://rust-lang.github.io/rust-clippy/master/index.html#absurd_extreme_comparisons\">Clippy website</a>.");
         assertThat(rule.tags()).isEmpty();
