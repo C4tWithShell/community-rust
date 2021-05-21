1018f639ff69c68d87dd80fe374018569d7a8b39
Eric Le Goff <elegoff@protonmail.com>
2021-05-21 18:10:00 +0200

    inner attributes in two lines

--- rust-frontend/src/main/java/org/sonar/rust/RustGrammar.java
+++ rust-frontend/src/main/java/org/sonar/rust/RustGrammar.java
@@ -345,7 +345,7 @@ public enum RustGrammar implements GrammarRuleKey {
         LexerlessGrammarBuilder b = LexerlessGrammarBuilder.create();
 
 
-        b.rule(COMPILATION_UNIT).is(SPC, b.zeroOrMore(INNER_ATTRIBUTE), SPC, b.zeroOrMore(STATEMENT, SPC), EOF);
+        b.rule(COMPILATION_UNIT).is(SPC, b.zeroOrMore(INNER_ATTRIBUTE,SPC), SPC, b.zeroOrMore(STATEMENT, SPC), EOF);
 
         punctuators(b);
         keywords(b);
