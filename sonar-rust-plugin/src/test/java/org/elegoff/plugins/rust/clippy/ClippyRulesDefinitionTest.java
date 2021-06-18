4900a00359672d30fe21879c6e13afa1f79df544
Eric Le Goff <elegoff@protonmail.com>
2021-06-18 10:42:58 +0200

    fix unit test

--- sonar-rust-plugin/src/test/java/org/elegoff/plugins/rust/clippy/ClippyRulesDefinitionTest.java
+++ sonar-rust-plugin/src/test/java/org/elegoff/plugins/rust/clippy/ClippyRulesDefinitionTest.java
@@ -38,7 +38,7 @@ public class ClippyRulesDefinitionTest {
         assertThat(repository.name()).isEqualTo("Clippy");
         assertThat(repository.language()).isEqualTo("rust");
         assertThat(repository.isExternal()).isTrue();
-        assertThat(repository.rules().size()).isEqualTo(460);
+        assertThat(repository.rules().size()).isEqualTo(462);
 
         RulesDefinition.Rule rule = repository.rule("clippy::absurd_extreme_comparisons");
         assertThat(rule).isNotNull();
