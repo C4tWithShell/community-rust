28c4e9a3698cd9ea443d08f33294489ac132f420
Eric Le Goff <elegoff@protonmail.com>
2021-07-23 18:38:52 +0200

    Add unit test for LineLength rule

--- rust-checks/src/test/java/org/elegoff/rust/checks/LineLengthCheckTest.java
+++ rust-checks/src/test/java/org/elegoff/rust/checks/LineLengthCheckTest.java
@@ -1,2 +1,19 @@
-package org.elegoff.rust.checks;public class LinLengthCheckTest {
+package org.elegoff.rust.checks;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class LineLengthCheckTest {
+    @Test
+    public void test() {
+        RustCheckVerifier.verify(new File("src/test/resources/checks/lineLength.rs"), new LineLengthCheck());
+    }
+
+    @Test
+    public void custom() {
+        LineLengthCheck check = new LineLengthCheck();
+        check.maximumLineLength = 119;
+        RustCheckVerifier.verify(new File("src/test/resources/checks/lineLength119.rs"), check);
+    }
 }
