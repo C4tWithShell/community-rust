ad6ac8c05a5926c5653eafef4d003d47fcd7f5a0
tfalque.ext <thibault_falque@ens.univ-artois.fr>
2022-01-12 10:14:01 +0100

    license header

--- community-rust-plugin/src/main/java/org/elegoff/plugins/communityrust/xunit/StaxParser.java
+++ community-rust-plugin/src/main/java/org/elegoff/plugins/communityrust/xunit/StaxParser.java
@@ -1,6 +1,25 @@
+/**
+ * Community Rust Plugin
+ * Copyright (C) 2021 Eric Le Goff
+ * mailto:community-rust AT pm DOT me
+ * http://github.com/elegoff/sonar-rust
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.elegoff.plugins.communityrust.xunit;
 
-import com.ctc.wstx.stax.WstxInputFactory;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.staxmate.SMInputFactory;
 import org.codehaus.staxmate.in.SMHierarchicCursor;
--- community-rust-plugin/src/main/java/org/elegoff/plugins/communityrust/xunit/TestSuite.java
+++ community-rust-plugin/src/main/java/org/elegoff/plugins/communityrust/xunit/TestSuite.java
@@ -1,3 +1,23 @@
+/**
+ * Community Rust Plugin
+ * Copyright (C) 2021 Eric Le Goff
+ * mailto:community-rust AT pm DOT me
+ * http://github.com/elegoff/sonar-rust
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.elegoff.plugins.communityrust.xunit;
 
 import java.util.ArrayList;
@@ -33,18 +53,4 @@ public class TestSuite {
     public List<TestCase> getTestCases() {
         return testCases;
     }
-
-//    /**
-//     * Returns execution details as sonar-conform XML
-//     */
-//    public String getDetails() {
-//        StringBuilder details = new StringBuilder();
-//        details.append("<tests-details>");
-//        for (TestCase tc : testCases) {
-//            details.append(tc.getDetails());
-//        }
-//        details.append("</tests-details>");
-//        return details.toString();
-//    }
-
 }
