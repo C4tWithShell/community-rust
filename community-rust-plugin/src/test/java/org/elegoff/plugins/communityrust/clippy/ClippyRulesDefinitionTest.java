409ed2ba56634226aa91f77825ee3197a03d08b3
Eric Le Goff <elegoff@protonmail.com>
2022-04-05 15:29:02 +0200

    Refresh Clippy list

--- community-rust-plugin/src/test/java/org/elegoff/plugins/communityrust/clippy/ClippyRulesDefinitionTest.java
+++ community-rust-plugin/src/test/java/org/elegoff/plugins/communityrust/clippy/ClippyRulesDefinitionTest.java
@@ -27,25 +27,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClippyRulesDefinitionTest {
 
-    @Test
-    public void clippyExternalRepository() {
-        RulesDefinition.Context context = new RulesDefinition.Context();
-        ClippyRulesDefinition rulesDefinition = new ClippyRulesDefinition();
-        rulesDefinition.define(context);
+  @Test
+  public void clippyExternalRepository() {
+    RulesDefinition.Context context = new RulesDefinition.Context();
+    ClippyRulesDefinition rulesDefinition = new ClippyRulesDefinition();
+    rulesDefinition.define(context);
 
-        assertThat(context.repositories()).hasSize(1);
-        RulesDefinition.Repository repository = context.repository("external_clippy");
-        assertThat(repository).isNotNull();
-        assertThat(repository.name()).isEqualTo("Clippy");
-        assertThat(repository.language()).isEqualTo("rust");
-        assertThat(repository.isExternal()).isTrue();
-        assertThat(repository.rules().size()).isEqualTo(501);
+    assertThat(context.repositories()).hasSize(1);
+    RulesDefinition.Repository repository = context.repository("external_clippy");
+    assertThat(repository).isNotNull();
+    assertThat(repository.name()).isEqualTo("Clippy");
+    assertThat(repository.language()).isEqualTo("rust");
+    assertThat(repository.isExternal()).isTrue();
+    assertThat(repository.rules().size()).isEqualTo(518);
 
-        RulesDefinition.Rule rule = repository.rule("clippy::absurd_extreme_comparisons");
-        assertThat(rule).isNotNull();
-        assertThat(rule.name()).isEqualTo("Checks for comparisons where one side of the relation is either the minimum or maximum value for its");
-        assertThat(rule.htmlDescription()).isEqualTo("See description of Clippy rule <code>clippy::absurd_extreme_comparisons</code> at" +
-                " the <a href=\"https://rust-lang.github.io/rust-clippy/master/index.html#absurd_extreme_comparisons\">Clippy website</a>.");
-        assertThat(rule.tags()).isEmpty();
-    }
+    RulesDefinition.Rule rule = repository.rule("clippy::absurd_extreme_comparisons");
+    assertThat(rule).isNotNull();
+    assertThat(rule.name()).isEqualTo("Checks for comparisons where one side of the relation is either the minimum or maximum value for its");
+    assertThat(rule.htmlDescription()).isEqualTo("See description of Clippy rule <code>clippy::absurd_extreme_comparisons</code> at" +
+      " the <a href=\"https://rust-lang.github.io/rust-clippy/master/index.html#absurd_extreme_comparisons\">Clippy website</a>.");
+    assertThat(rule.tags()).isEmpty();
+  }
 }
