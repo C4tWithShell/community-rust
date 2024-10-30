925dc4a23c3c2cb2e97a80fbd1cd408222f033d3
Vladimir Shelkovnikov <99356504+C4tWithShell@users.noreply.github.com>
2024-10-30 20:34:44 +0300

    v0.2.5 (#115)
    * Update notice, dependencies version, deprecate FileAdjust, update script to update clippy rules, update tests
    
    * Update readme
    
    * Adjust rule length

--- utils/clippy-lints-import.py
+++ utils/clippy-lints-import.py
@@ -1,42 +1,41 @@
 #!/usr/local/bin/python3
 
 import urllib.request, json
-clippy_lints_url = "https://rust-lang.github.io/rust-clippy/master/lints.json"
+from bs4 import BeautifulSoup
+
+clippy_lints_url = "https://rust-lang.github.io/rust-clippy/master/index.html"
 
 
-def get_name_from_docstring(docstring):
-    length = 100
-    start_index = len(" ### What it does\n")
-    s = docstring[start_index:].replace('\n',' ')
-    idx = s.find('###')
-    if idx < 100:
-        length=idx
-    return s[:length].strip()
+def get_name_from_docstring(docs):
+    paragraph = docs.find('p')
+    for code_tag in paragraph.find_all('code'):
+        code_tag.replace_with(f"`{code_tag.text}`")
+    docstring = paragraph.get_text()
+    s = docstring.replace('\n',' ')
+    s = s[:100] if len(s) > 101 else s
+    return s.strip()
 
 
 
 with urllib.request.urlopen(clippy_lints_url) as url:
-    data = json.loads(url.read().decode())
+    html = BeautifulSoup(url.read().decode(), 'html.parser')
+    articles = html.find_all('article')
     
 rules = []
 
+for rule in articles:
+    deprecated = rule.find(class_='label-group-deprecated')
+    if (deprecated):
+        continue
+    id = rule.find(class_='panel-title-name')['id'].replace('lint-','')
+    key = "clippy::" + id
+    name = get_name_from_docstring(rule.find(class_='lint-docs'))
+    url="https://rust-lang.github.io/rust-clippy/master/index.html#"+id
 
-
-for lint in data:
-
-   if lint["group"] == "deprecated":
-       continue
-
-   id =  lint["id"]
-   key="clippy::" + id
-   name = get_name_from_docstring(lint["docs"])
-   url="https://rust-lang.github.io/rust-clippy/master/index.html#"+id
-   sonar_rule={}
-   sonar_rule["key"] = key
-   sonar_rule["name"] = name
-   sonar_rule["url"] = url
-   rules.append(sonar_rule)
+    sonar_rule={}
+    sonar_rule["key"] = key
+    sonar_rule["name"] = name
+    sonar_rule["url"] = url
+    rules.append(sonar_rule)
 
 print(json.dumps(rules))
-
-
